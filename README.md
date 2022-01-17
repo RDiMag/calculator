5b55bac0bc3b724474602511a5be05320efeabb2
Rachel <proofreader_one@yahoo.com>
2022-01-17 11:31:17 -0500

    add, subtract, multiply, and divide functions

---
+++ README.md
@@ -0,0 +1,7 @@
+Calculator
+
+This project will contain functions for basic arithmetic: add, subtract, multiply, and divide.
+
+It will contain an operate function that takes an operator and 2 numbers and calls one of the basic arithmetaic functions listed above.
+
+The interface will be in basic HTML.
---
+++ index.js
@@ -0,0 +1,24 @@
+const add = function(numA, numB) {
+    let total = (numA + numB);
+    console.log(total);
+      return total;
+  };
+  
+  const subtract = function(numA, numB) {
+      let subtractedTotal = (numA - numB);
+      console.log(subtractedTotal);
+      return (subtractedTotal);
+  };
+  
+  
+  const multiply = function(numA, numB) {
+    let result = (numA * numB);
+    console.log(result);
+    return result;
+  };
+
+  const divide = function (numA, numB) {
+    let dividend = (numA / numB);
+    console.log(dividend);
+    return dividend;
+  };
