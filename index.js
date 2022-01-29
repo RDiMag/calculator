8b447d421c7e28e537ef3381b068232cd5fb9fa9
Rachel <proofreader_one@yahoo.com>
2022-01-28 20:37:03 -0500

    working Operator function

--- index.js
+++ index.js
@@ -17,8 +17,32 @@ const add = function(numA, numB) {
     return result;
   };
 
-  const divide = function (numA, numB) {
+  const divide = function(numA, numB) {
     let dividend = (numA / numB);
     console.log(dividend);
     return dividend;
+  };
+
+  const operate = function(operator, numA, numB) {
+    let operation = 0;
+    if (operator === '+') {
+      operation = add(numA, numB);
+      console.log(operation);
+      return;
+    } else if (operator === '-') {
+      operation = subtract(numA, numB);
+      console.log(operation);
+      return;
+    } else if (operator === '*') {
+      operation = multiply(numA, numB);
+      console.log(operation);
+      return;
+    } else if (operator === '/') {
+      if (numA && numB != 0) {
+        operation = divide(numA, numB);
+        console.log(operation);
+        return;
+      }
+        else alert('Cannot divide by zero');
+      }
   };
